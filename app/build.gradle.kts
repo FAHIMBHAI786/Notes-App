ac496ec5f04e538b932f8471e6a304bed28ee7f6
[200~FAHIMBHAI786~ <fahimislam7860@gmail.com>
2023-09-13 17:07:37 +0530

    Gradle Problem solved

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.example.mynotesapplication"
-        minSdk = 34
+        minSdk = 26
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
